474ea7a547d60da71c1b75e682852da6f3687c09
soomingjin <mingjin_soo@mymail.sutd.edu.sg>
2018-04-03 16:07:05 +0800

    added three.js

---
+++ js/main.js
@@ -0,0 +1,15 @@
+console.log("main.js");
+
+let scene, camera, renderer;
+
+
+init();
+animate();
+
+function init() {
+  console.log("init");
+}
+
+function animate() {
+  console.log("animate");
+}
